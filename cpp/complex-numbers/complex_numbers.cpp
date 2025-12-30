f48156280213ad7e0ae584f3ac168dfa6df75dea
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-12-30 23:56:05 +0100

    feat: Implement complex number class with all basic artimetic operations. Support as well operations on real and complex numbers.

---
+++ cpp/complex-numbers/complex_numbers.cpp
@@ -0,0 +1,108 @@
+#include "complex_numbers.h"
+
+#include <cmath>
+#include <stdexcept>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+#ifndef M_E
+#define M_E 2.71828182845904523536
+#endif
+
+namespace complex_numbers {
+    Complex::Complex(double a, double b) : a(a), b(b) {
+    };
+
+    Complex Complex::operator+(const Complex &other) const {
+        return {a + other.a, b + other.b};
+    }
+
+    Complex Complex::operator+(double x) const {
+        return {a + x, b};
+    }
+
+
+    Complex Complex::operator-(const Complex &other) const {
+        return {a - other.a, b - other.b};
+    }
+
+    Complex Complex::operator-(double x) const {
+        return {a - x, b};
+    }
+
+    Complex Complex::operator*(const Complex &other) const {
+        return {a * other.a - b * other.b, b * other.a + a * other.b};
+    }
+
+    Complex Complex::operator*(double x) const {
+        return {a * x, b * x};
+    }
+
+    Complex Complex::operator/(const Complex &other) const {
+        //z1=a+bi
+        //z2=c+di
+        double c = other.a;
+        double d = other.b;
+
+        double denom = c * c + d * d;
+
+        if (denom == 0.0) {
+            throw std::domain_error("Complex division by 0+0i.");
+        }
+        double real = (a * c + b * d) / denom;
+        double imag = (b * c - a * d) / denom;
+
+        return {real, imag};
+    }
+
+    Complex Complex::operator/(double x) const {
+        return {a / x, b / x};
+    }
+
+    Complex Complex::conj() const {
+        return {a, -b};
+    }
+
+    double Complex::modulusSquare() const {
+        return a * a + b * b;
+    }
+
+    double Complex::abs() const {
+        return sqrt(a * a + b * b);
+    }
+
+    double Complex::real() const {
+        return a;
+    }
+
+    double Complex::imag() const {
+        return b;
+    }
+
+    Complex Complex::exp() const {
+        double real = std::pow(M_E, a) * cos(b);
+        double imag = std::pow(M_E, a) * sin(b);
+        return {real, imag};
+    }
+
+    //non-member function
+    Complex operator+(double x, const Complex &z) {
+        return {x + z.a, z.b};
+    }
+
+    Complex operator-(double x, const Complex &z) {
+        return {x - z.a, -z.b};
+    }
+
+    Complex operator*(double x, const Complex &z) {
+        return {x * z.a, x * z.b};
+    }
+
+    Complex operator/(double x, const Complex &z) {
+        double denom = z.a * z.a + z.b * z.b;
+        double real = x * z.a / denom;
+        double imag = -x * z.b / denom;
+        return {real, imag};
+    }
+} // namespace complex_numbers
---
+++ cpp/complex-numbers/complex_numbers.h
@@ -0,0 +1,47 @@
+#pragma once
+
+namespace complex_numbers {
+    class Complex {
+    public:
+        double a;
+        double b;
+
+        Complex(double a = 0.0, double b = 0.0);
+
+        Complex operator+(const Complex &other) const;
+
+        Complex operator+(double x) const;
+
+        Complex operator*(const Complex &other) const;
+
+        Complex operator*(double x) const;
+
+        Complex operator-(const Complex &other) const;
+
+        Complex operator-(double x) const;
+
+        Complex operator/(const Complex &other) const;
+
+        Complex operator/(double x) const;
+
+        Complex conj() const;
+
+        double modulusSquare() const;
+
+        double abs() const;
+
+        double real() const;
+
+        double imag() const;
+
+        Complex exp() const;
+    };
+
+    Complex operator+(double x, const Complex &z);
+
+    Complex operator-(double x, const Complex &z);
+
+    Complex operator*(double x, const Complex &z);
+
+    Complex operator/(double x, const Complex &z);
+} // namespace complex_numbers
